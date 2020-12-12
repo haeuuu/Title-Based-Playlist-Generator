01b7ffec6946ac4977114da1b26ce9f49980b9f0
haeuuu <srhuu15@naver.com>
2020-12-12 23:34:52 +0900

    fix : Fix typo

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
-from MelonplyBuilder import *
+from MelonPlyBuilder import *
 from TitleBasedPlyGenerator import *
 
 dir = 'arena_data'
 
-title = input("오늘은 이런 노래가 땡긴다 (~v~)/ : ")
-
 ply_generator = TitleBasedPlyGenerator(dir)
 ply_generator.set_default()
-rec_songs, extracted_tags, rec_tags = ply_generator.title_based_recommend(title, topk = 20)
+
+title = input("오늘은 이런 노래가 땡긴다 (~v~)/ : ")
+rec_songs, extracted_tags, rec_tags = ply_generator.title_based_recommend(title, topk = 50)
 play_list = [title] + ply_generator.convert_to_name(rec_songs)
 
 ply_generator = MelonPlyBuilder()
