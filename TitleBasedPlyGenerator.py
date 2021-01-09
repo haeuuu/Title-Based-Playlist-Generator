1aff9e57a9c955a9db1ae264c3669f187be59a0b
haeuuu <srhuu15@naver.com>
2021-01-09 23:58:13 +0900

    update : 모든 태그가 아닌 명사만 추출하도록 수정

--- TitleBasedPlyGenerator.py
+++ TitleBasedPlyGenerator.py
@@ -2,6 +2,13 @@ from itertools import combinations
 import re, os
 import pandas as pd
 import pickle
+from konlpy.tag import Hannanum
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = {}
+        self.is_terminal = False
 
 class Node:
     def __init__(self, value):
@@ -10,15 +17,26 @@ class Node:
         self.is_terminal = False
 
 class Trie:
-    def __init__(self, items):
+    def __init__(self, w2v_model):
+        self.w2v_model = w2v_model
         self.head = Node(None)
 
         print("********* DB 구성중입니다 *********")
-        for item in items:
-            self.insert(item)
-        print("*********** 입력 완료 ************")
+        for word in w2v_model.wv.vocab.keys():
+            if word.isdigit():
+                continue
+            word = ''.join(self.resub(word))
+            if word:
+                self.insert(word)
+        print("************ 입력 완료 ************")
+
+    def resub(self, query):
+        return re.findall('\w+', query)
 
     def insert(self, query):
+        if len(query) <= 1:
+            return
+
         curr_node = self.head
 
         for q in query:
@@ -27,39 +45,58 @@ class Trie:
             curr_node = curr_node.children[q]
         curr_node.is_terminal = query
 
-    def extract(self, query, biggest_token=True):  # two pointer로 만들어야되는구나 ㅠㅠ
-        query += '***'  # padding
+    def search(self, query):
+        curr_node = self.head
+
+        for q in query:
+            curr_node = curr_node.children.get(q)
+            if curr_node is None:
+                return False
+
+        if curr_node.is_terminal:
+            return True
+        return False
+
+    def extract(self, query, biggest_token = True):
+        start, end = 0,0
+        query += '*'
         curr_node = self.head
         prev_node = self.head
-        start = 0
 
         extracted_tags = []
-
-        i = 0  # 현재 위치
-        while i < len(query):
-            curr_node = curr_node.children.get(query[i])
-
+        while end < len(query):
+            curr_node = curr_node.children.get(query[end])
             if curr_node is None:
                 if biggest_token and prev_node.is_terminal:
                     extracted_tags.append(prev_node.is_terminal)
-                    # 단어를 발견했으면 start를 jump시키자 => 드라이브 => 드라이브, 이브 와 같은 경우 방지
-                    start = i
-                else:
-                    # 발견하지 못했다면 start를 한칸만 옮기자 => 해변가/로드/라이브 => 해변가, 라이브 와 같은 경우 방지
-                    # (jump 시켜버리면 로드트립을 찾고 실패한 후 '라'부터 탐색을 시작해서 드라이브 못찾음.)
-                    start += 1
-                i = start
+                    start = end-1
+                    prev_node = self.head
+                start += 1
+                end = start
                 curr_node = self.head
-                prev_node = self.head
-
             else:
-                if curr_node.is_terminal:
-                    if not biggest_token:
-                        extracted_tags.append(curr_node.is_terminal)
+                if not biggest_token and curr_node.is_terminal:
+                    extracted_tags.append(curr_node.is_terminal)
+                elif curr_node.is_terminal:
                     prev_node = curr_node
-                i += 1
+                end += 1
 
-        return list(set(extracted_tags))
+        return  extracted_tags
+
+    def extract_from_title(self, title, biggest_token = True):
+        tags = []
+        for word in title.split():
+            tags.extend(self.extract(word,biggest_token))
+        return tags
+
+    def extract_nouns(self, title):
+        nouns = hannanum.nouns(" ".join(self.resub(title)))
+        results = []
+        for noun in nouns:
+            if self.search(noun):
+                results.append(noun)
+
+        return results
 
 class TitleBasedPlyGenerator:
     def __init__(self, dir):
@@ -143,7 +180,7 @@ class TitleBasedPlyGenerator:
     def title_based_recommend(self, title, topk = 20 ,verbose=True, biggest_token=True):
         extracted_items = self.extract_tags(sentence = title, verbose = verbose, biggest_token = biggest_token)
         rec_songs, rec_tags = self.tag_based_recommend(extracted_items, topk = topk, verbose = verbose)
-        return rec_songs, extracted_tags, rec_tags
+        return rec_songs, extracted_items, rec_tags
 
     def tag_based_recommend(self, tags, topk = 20 ,verbose=True):
         rec_songs, rec_tags = self.top_items(tags, verbose)
@@ -154,7 +191,7 @@ class TitleBasedPlyGenerator:
         return rec_songs[:topk], rec_tags[:10]
 
 if __name__ == '__main__':
-    dir = 'arena_data'
+    dir = r'C:\Users\haeyu\PycharmProjects\KakaoArena\arena_data\model'
     ply_generator = TitleBasedPlyGenerator(dir)
     ply_generator.set_default()
 
