14bc7e90e101f9b9baecbc9971fb9aa22b1a9de1
haeuuu <srhuu15@naver.com>
2021-02-02 16:06:10 +0900

    update : TitleBasedPlyGenerator 변경 사항에 맞게 수정

--- run.py
+++ run.py
@@ -1,19 +1,47 @@
 from MelonPlyBuilder import *
 from TitleBasedPlyGenerator import *
 
-dir = r'C:\Users\haeyu\PycharmProjects\KakaoArena\arena_data\model'
+"""
+Examples
 
-""" Examples : 청량한 여름, 듣고 싶은 노래들 / 편집샵 주인장 플레이 리스트 훔쳐왔다 !!! / 재즈 속에서 와인 한 잔 """
-title = input("오늘은 이런 노래가 땡긴다 (~v~)/ : ")
+청량한 남돌 노래만 차곡차곡
+틀어놓기만 해도 해변가 드라이브 중
+편집샵 주인장 플레이 리스트 훔쳐왔다!!
+방구석 스타벅스 만들어주는 플레이리스트
+마음이 몽글몽글해지는 팝송
+추적추적 비내리는 창가
+도입부 장난 아님
 
-ply_generator = TitleBasedPlyGenerator(dir)
-ply_generator.set_default()
-rec_songs, extracted_tags, rec_tags = ply_generator.title_based_recommend(title, topk = 30)
-play_list = [title] + ply_generator.convert_to_name(rec_songs)
+"""
 
-ply_generator = MelonPlyBuilder()
+dir = r'C:\Users\haeyu\PycharmProjects\KakaoArena\arena_data'
+mode = 'bm25'
+
+train_path = os.path.join(dir, 'orig', 'train.json')
+val_path = os.path.join(dir, 'orig', 'val.json')
+song_meta_path = os.path.join(dir, 'model', 'song_meta_sub.pkl')
+w2v_model_path = os.path.join(dir, 'model', 'w2v_128.pkl')
+
+ply_generator = TitleBasedRecommender(train_path, val_path, w2v_model_path, song_meta_path)
+
+p2v_model_path = os.path.join(dir, 'model', f'p2v_model_{mode}.pkl')
+if f'p2v_model_{mode}.pkl' in os.listdir(os.path.join(dir, 'model')):
+    ply_generator.register_p2v(p2v_model_path)
+else:
+    ply_generator.build_p2v(mode=mode, path_to_save=dir)
+
 uid, password = input("Insert your Melon ID : "), input("Insert your Melon pwd : ")
-ply_generator.login(uid, password)
-ply_generator.run(play_list)
-ply_generator.logout()
-ply_generator.quit()
+ply_builder = MelonPlyBuilder()
+ply_builder.login(uid, password)
+
+while True:
+    title = input("\n오늘은 이런 노래가 땡긴다 ~ (~v~)/ : ")
+    if title == 'quit':
+        print('\n이용해주셔서 감사함니다 ~ (-v-) (_ _)')
+        break
+    rec_songs, rec_tags = ply_generator.recommend(title, topn = 30, mode = mode)
+    play_list = [title] + ply_generator.convert_to_name(rec_songs)
+    ply_builder.run(play_list)
+
+ply_builder.logout()
+ply_builder.quit()
