70ca3fba69744196714a24903865e733dfdc0eff
haeuuu <srhuu15@naver.com>
2021-02-03 15:46:56 +0900

    update : __all__ 추가

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
+from .util import load_json
 from .weighted_ratings import Ratings
 from .extract_tags import TagExtractor
 from .melon_ply_builder import MelonPlyBuilder
 from .Playlist2Vec import Playlist2Vec
 from .TitleBasedPlyGenerator import TitleBasedRecommender
+
+__all__ = ['TitleBasedRecommender','MelonPlyBuilder','Playlist2Vec']
