b3bc68da6e5c8de7ea5fa678e7b06e31fd42d306
haeuuu <srhuu15@naver.com>
2021-02-08 15:43:03 +0900

    feature : tag 거르기 위한 limit 변수 추가

--- extract_tags.py
+++ extract_tags.py
@@ -10,19 +10,20 @@ class TagExtractor:
     """
     vocab에 들어있는 단어만을 추출한다.
     """
-    def __init__(self):
+    def __init__(self, limit = 2):
         self.head = Node(None)
+        self.limit = limit
 
     def build_by_vocab(self, words):
         for word in words:
-            self.insert(word)
+            self.insert(word, self.limit)
 
     def build_by_w2v(self, w2v_model_path):
         with open(w2v_model_path, 'rb') as f:
             w2v_model = pickle.load(f)
 
         for word in w2v_model.wv.vocab.keys():
-            self.insert(word)
+            self.insert(word, self.limit)
 
     def not_satisfied(self, query, limit = 1):
         """
@@ -36,8 +37,8 @@ class TagExtractor:
             return len(query) <= limit and query.encode().isalpha()
         return len(query) < limit
 
-    def insert(self, query):
-        if self.not_satisfied(query):
+    def insert(self, query, limit):
+        if self.not_satisfied(query, limit):
             return
 
         curr_node = self.head
@@ -95,10 +96,10 @@ class TagExtractor:
 
     def convert(self, query, merged = False):
         """
-        vocab : 잔잔한, 감성
-        input : 잔잔한감성 입니당
-        extracted : [ 잔잔한 , 감성 ] ( if merged == True, '잔잔한감성' 자체가 vocab에 없으므로 추가. )
-        return : [ 잔잔한 , 감성 , 입니당 ] ( if merged == True, [ 잔잔한, 감성, 잔잔한감성, 입니당 ] )
+        vocab : Christmas, 잔잔한, 감성
+        input : christmas 잔잔한감성 입니당
+        extracted : [ Christmas, 잔잔한 , 감성 ] ( if merged == True, '잔잔한감성' 자체가 vocab에 없으므로 추가. )
+        return : [ Christmas, 잔잔한 , 감성 , 입니당 ] ( if merged == True, [ Christmas, 잔잔한, 감성, 잔잔한감성, 입니당 ] )
         """
         res = []
         for q in query.split():
@@ -108,7 +109,7 @@ class TagExtractor:
                 if merged and not self.search(q):
                         res.append(q.lower())
             else:
-                if not self.not_satisfied(q):
+                if not self.not_satisfied(q, self.limit):
                     res.append(q)
 
         return res
